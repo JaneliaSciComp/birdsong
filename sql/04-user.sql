07bce9aa6d790d85ef0d7e47e420deae2260634a
robsv <robsvi@gmail.com>
2022-11-18 15:03:09 -0500

    Fixed user creates

--- sql/04-user.sql
+++ sql/04-user.sql
@@ -2,11 +2,11 @@
 -- Create Users  --
 -- ============= --
 -- CREATE USER 'birdAdmin'@'%' IDENTIFIED WITH mysql_native_password BY 'birdAdmin'; --
-DROP USER birdAdmin;
+DROP USER IF EXISTS birdAdmin;
 CREATE USER 'birdAdmin'@'%' IDENTIFIED BY 'birdAdmin';
-DROP USER birdApp;
+DROP USER IF EXISTS birdApp;
 CREATE USER 'birdApp'@'%' IDENTIFIED BY 'birdApp';
-DROP USER birdRead;
+DROP USER IF EXISTS birdRead;
 CREATE USER 'birdRead'@'%' IDENTIFIED BY 'birdRead';
 
 -- ====================== --
