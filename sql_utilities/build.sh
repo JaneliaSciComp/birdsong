ebde212ba6c6cd176bdef6db1b916ce950172c6d
robsv <robsvi@gmail.com>
2022-11-18 15:57:26 -0500

    Resequenced SQL commands

--- sql_utilities/build.sh
+++ sql_utilities/build.sh
@@ -2,14 +2,14 @@ mysql -h localhost -u root -proot <01-dbBuild.sql
 echo "Creating tables"
 mysql -h localhost -u root -proot birdsong <02-schema.sql 
 echo "Adding users"
-mysql -h localhost -u root -proot birdsong <04-user.sql 
+mysql -h localhost -u root -proot birdsong <03-user.sql 
 echo "Defining functions"
-mysql -h localhost -u root -proot birdsong <05-getCvId-function.sql 
-mysql -h localhost -u root -proot birdsong <05-getCvTermDefinition-function.sql 
-mysql -h localhost -u root -proot birdsong <05-getCvTermDisplayName-function.sql 
-mysql -h localhost -u root -proot birdsong <05-getCvTermId-function.sql         
-mysql -h localhost -u root -proot birdsong <05-getCvTermName-function.sql 
+mysql -h localhost -u root -proot birdsong <04-getCvId-function.sql 
+mysql -h localhost -u root -proot birdsong <04-getCvTermDefinition-function.sql 
+mysql -h localhost -u root -proot birdsong <04-getCvTermDisplayName-function.sql 
+mysql -h localhost -u root -proot birdsong <04-getCvTermId-function.sql         
+mysql -h localhost -u root -proot birdsong <04-getCvTermName-function.sql 
 echo "Defining views"
-mysql -h localhost -u root -proot birdsong <03-views.sql 
+mysql -h localhost -u root -proot birdsong <05-views.sql 
 echo "Populating"
 mysql -h localhost -u root -proot birdsong <99-initial_load.sql
