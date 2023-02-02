c123bc4a970416d6db68328be7db17345c6ecd04
Robert R Svirskas <svirskasr@vmdmz176.int.janelia.org>
2023-02-02 15:46:38 +0000

    db creds

--- sql/03-user.sql
+++ sql/03-user.sql
@@ -3,17 +3,17 @@
 -- ============= --
 -- CREATE USER 'birdAdmin'@'%' IDENTIFIED WITH mysql_native_password BY 'birdAdmin'; --
 DROP USER IF EXISTS birdAdmin;
-CREATE USER 'birdAdmin'@'%' IDENTIFIED BY 'birdAdmin';
+CREATE USER 'birdAdmin'@'%' IDENTIFIED BY '313bldsaf560';
 DROP USER IF EXISTS birdApp;
-CREATE USER 'birdApp'@'%' IDENTIFIED BY 'birdApp';
+CREATE USER 'birdApp'@'%' IDENTIFIED BY 's0ciety_mbl_23';
 DROP USER IF EXISTS birdRead;
-CREATE USER 'birdRead'@'%' IDENTIFIED BY 'birdRead';
+CREATE USER 'birdRead'@'%' IDENTIFIED BY 's0cietyRead';
 
 -- ====================== --
 -- Apply User Privileges
 -- ====================== --
 
-GRANT ALL PRIVILEGES ON birdsong.* TO birdAdmin@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO birdAdmin@'%' WITH GRANT OPTION;
 GRANT SUPER ON *.* TO birdAdmin@'%' WITH GRANT OPTION;
-GRANT SELECT,INSERT,UPDATE,DELETE,EXECUTE,SHOW VIEW ON birdsong.* TO birdApp@'%';
-GRANT SELECT,SHOW VIEW,EXECUTE ON birdsong.* TO birdRead@'%';
+GRANT SELECT,INSERT,UPDATE,DELETE,EXECUTE,SHOW VIEW ON *.* TO birdApp@'%';
+GRANT SELECT,SHOW VIEW,EXECUTE ON *.* TO birdRead@'%';
