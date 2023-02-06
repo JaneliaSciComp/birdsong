2f7603d7085a3d0eeea43951e1015502b6c91ea4
Robert R Svirskas <svirskasr@vmdmz176.int.janelia.org>
2023-02-06 19:54:22 +0000

    Docker

--- restart_production.sh
+++ restart_production.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker-compose -f docker-compose-prod.yml down
-docker image ls | grep registry.int.janelia.org | awk '{print $3}' | xargs docker image rm
-docker volume rm assignment-manager_static_volume
-docker pull registry.int.janelia.org/flyem/assignment-manager
-docker-compose -f docker-compose-prod.yml up
+sudo docker-compose -f docker-compose.yml down
+sudo docker image ls | grep birdsong_app | awk '{print $3}' | xargs sudo docker image rm
+#sudo docker volume rm assignment-manager_static_volume
+##sudo docker pull registry.int.janelia.org/flyem/assignment-manager
+#sudo docker-compose -f docker-compose.yml up
